e1b5e2766d40ab596cf43a069faf3b3e7a98a346
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-23 20:08:45 +0900

    React App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import client from './client'
 import { ApolloProvider } from "react-apollo";
 import { HashRouter as Router , Route } from "react-router-dom";
-import {Image , User_Image} from './image'
-import {Comment , Image_Comment} from './comment'
-import {Like , Image_Like} from './like'
-import {Followers ,Followings } from "./follow";
-import { User_Page , Profile } from "./user";
+import {Image , User_Image , Create_Image} from './image'
+import {Comment , Image_Comment, Create_Comment} from './comment'
+import {Like , Image_Like , Create_Like} from './like'
+import {Followers ,Followings , Create_Follower } from "./follow";
+import { User_Page , Profile , Create_User } from "./user";
 
 class App extends Component {
   render() {
@@ -19,11 +19,16 @@ class App extends Component {
             <Route exact={true} path={"/comment/:image_id"} component={Image_Comment} />
             <Route exact={true} path={"/like/"} component={Like} />
             <Route exact={true} path={"/like/:image_id"} component={Image_Like} />
-            <Route exact={true} path={"/image/:user_id"} component={User_Image} />
-            <Route exact={true} path={"/follower/:from_id"} component={Followers} />
-            <Route exact={true} path={"/following/:to_id"} component={Followings} />
+            <Route exact={true} path={"/image/:username"} component={User_Image} />
+            <Route exact={true} path={"/follower/:from_username"} component={Followers} />
+            <Route exact={true} path={"/following/:to_username"} component={Followings} />
             <Route exact={true} path={"/users/"} component={User_Page} />
             <Route exact={true} path={"/users/:username"} component={Profile} />
+            <Route exact={true} path={"/create/image"} component={Create_Image} />
+            <Route exact={true} path={"/create/comment"} component={Create_Comment} />
+            <Route exact={true} path={"/create/like"} component={Create_Like} />
+            <Route exact={true} path={"/create/user"} component={Create_User} />
+            <Route exact={true} path={"/create/follow"} component={Create_Follower} />
           </div> 
         </Router>
       </ApolloProvider>
