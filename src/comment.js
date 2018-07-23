b95f863da3b8abfa8618d263c3704cce6d6a2c29
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-23 20:09:26 +0900

    Apollo Comment Router

--- src/comment.js
+++ src/comment.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Query } from "react-apollo";
-import { Comment_page,Image_comment } from "./queries";
+import { Query, Mutation } from "react-apollo";
+import { Comment_page,Image_comment, Create_comment } from "./queries";
 
 export const Comment = () => 
     <Query query={Comment_page}>
@@ -11,7 +11,7 @@ export const Comment = () =>
                 <h2 key={comment.comment_id}>
                     image_id : {comment.image_id} /
                     Message : {comment.message} / 
-                    Comment_user_id : {comment.user_id}
+                    Comment_username : {comment.username}
                 </h2>)
         }}
     </Query>;
@@ -31,8 +31,41 @@ export const Image_Comment = (
                 <h2 key={comment.comment_id}>
                     image_id : {comment.image_id} /
                     Message : {comment.message} / 
-                    Comment_user_id : {comment.user_id}
+                    Comment_username : {comment.username}
                 </h2>
             )
         }}
     </Query>;
+
+
+export const Create_Comment = () => {
+    let image_id , username , message;
+    return(
+        <Mutation mutation={Create_comment}>
+            {(getComment , {loading , error }) => (
+                <div>
+                    <form onSubmit ={e => {
+                        e.preventDefault()
+                        getComment({
+                            variables : {
+                                image_id : image_id.value,
+                                username : username.value,
+                                message : message.value
+                            }
+                        })
+                        image_id.value = "";
+                        username.value = "";
+                        message.value = "";
+                    }}>
+                        <input type="text" placeholder="image_id" ref={node => {image_id = node}} /><br/><br/>
+                        <input type="text" placeholder="username" ref={node => {username = node}} /><br/><br/>
+                        <textarea placeholder="message" ref={node => {message = node}} /><br/><br/>
+                        <button type="submit">Create Comment</button>
+                    </form>
+                    {loading && <p>Loading...</p>}
+                    {error && <p>Error :( Please try again )</p>}    
+                </div>
+            )}
+        </Mutation>
+    )
+}
