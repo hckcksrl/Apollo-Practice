4730db369c87c07cf7c7e6d9e60173c236e1e3de
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-23 20:09:37 +0900

    Apollo Image Router

--- src/image.js
+++ src/image.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Query } from "react-apollo";
-import { Images_Page , User_image} from "./queries";
+import { Query , Mutation} from "react-apollo";
+import { Images_Page , User_image , Create_image} from "./queries";
 
 
 export const Image = () => <Query query={Images_Page}>
@@ -10,7 +10,7 @@ export const Image = () => <Query query={Images_Page}>
 		return data.allImage.map(image => 
 			<h2 key={image.image_id}>
 			image_file : {image.file} /
-			image_user_id : {image.user_id}
+			image_username : {image.username}
 			</h2>)
 	}}
 </Query>
@@ -18,18 +18,53 @@ export const Image = () => <Query query={Images_Page}>
 export const User_Image = (
 	{
 		match : {
-			params : user_id
+			params : username
 		}
 	}
-) => <Query query={User_image} variables={user_id}>
+) => <Query query={User_image} variables={username}>
 	{({loading , data , error}) =>{
 		if(loading) return 'loading'
 		if(error) return 'something happened'
 		return data.images.map(image => 
 			<h2 key={image.image_id}>
 				image_file : {image.file} /
-				image_user_id :  {image.user_id}
+				image_username :  {image.username}
 			</h2>
 		)
 	}}
 </Query>
+
+export const Create_Image = () =>{
+	let location , file , username
+	return(
+	<Mutation mutation={Create_image}>
+		{(getImage , {loading , data , error}) => 
+		(
+			<div>
+				<form onSubmit={e => {
+					e.preventDefault();
+					getImage(
+						{
+							variables : { 
+								location :location.value ,
+								file :file.value ,
+								username : username.value,
+							}
+						}
+					)
+					location.value = "";
+					file.value = "";
+					username.value = "";
+				}}>
+					<input type="text" placeholder="location" ref={node => {location = node}} /><br/><br/>
+					<input type="file" placeholder="file" ref={node => {file = node}} /><br/><br/>
+					<input type="text" placeholder="username" ref={node => {username = node}} /><br/><br/>
+					<button type="submit">Create Image</button>
+				</form>
+				{loading && <p>Loading...</p>}
+                {error && <p>Error :( Please try again )</p>}
+			</div>
+		)
+	}
+	</Mutation>
+)}
