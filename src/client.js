67f904e5de3419c335b9d22ea2694cb3e6d0329c
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-25 16:36:58 +0900

    Apollo Client

--- src/client.js
+++ src/client.js
@@ -1,10 +1,9 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import ApolloClient from "apollo-boost";
+
 
 const client = new ApolloClient({
-    link: new HttpLink({
-        uri : "http://localhost:4000",
-    }),
-    cache : new InMemoryCache()
+    uri : "http://localhost:4000",
+    cacheControl : false
 })
 
 export default client
